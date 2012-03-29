9a564a3e1a6e1ffee5db96ac351a741f87a2379c
Felix Tjandrawibawa <cemeng@gmail.com>
2012-03-29 22:37:49 +1100

    Ruby script to initialize vim bundles

---
+++ bin/init_vim_bundles.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+require 'open-uri'
+
+git_bundles = %w{
+  git://github.com/tpope/vim-fugitive.git
+  git://github.com/tpope/vim-git.git
+  git://github.com/tpope/vim-markdown.git
+  git://github.com/tpope/vim-rails.git
+  git://github.com/tpope/vim-surround.git
+  git://github.com/vim-ruby/vim-ruby.git
+  git://github.com/altercation/vim-colors-solarized.git
+  git://git.wincent.com/command-t.git
+}
+
+bundles_dir = "/Users/cemeng/.vim/bundle"
+
+FileUtils.rm_rf(bundles_dir)
+FileUtils.mkdir(bundles_dir)
+FileUtils.cd(bundles_dir)
+
+git_bundles.each do |url|
+  puts url
+  `git clone -q #{url}`
+end
+
+Dir["*/.git"].each {|f| FileUtils.rm_rf(f) }
